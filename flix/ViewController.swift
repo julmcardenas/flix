83492f7e87243f9689d2e834f8ec8fcfb2da7cc2
Julianna Cardenas <julmcardenas@gmail.com>
2021-01-10 18:46:25 -0800

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Julianna Cardenas on 1/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
