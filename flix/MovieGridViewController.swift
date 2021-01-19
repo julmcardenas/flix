3ce3676b69ef8aaeff26edba9ba5f20dd24bf4b9
Julianna Cardenas <julmcardenas@gmail.com>
2021-01-18 23:44:34 -0800

    Added Detail Screen for Grid

--- flix/MovieGridViewController.swift
+++ flix/MovieGridViewController.swift
@@ -64,14 +64,23 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         return cell
     }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        print("Loadin up the details screen")
+        
+        //Find the selected movie
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for:cell)!
+        let movie = movies[indexPath.row]
+        
+        //Pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieGridDetailsViewController
+        detailsViewController.movie = movie
+        
     }
-    */
 
 }
