ee37e39bd0a9c0008df8c81cd63f4dfe34b7a0ab
Julianna Cardenas <julmcardenas@gmail.com>
2021-01-18 21:30:05 -0800

    Added Superhero Grid Screen

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Julianna Cardenas on 1/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
